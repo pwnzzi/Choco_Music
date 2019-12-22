fb4053e1c57567e1e60b541d4ff22dc20ff61fe0
pwnzzi <pwnzzi@naver.com>
2019-12-23 02:28:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/choco_music/activities/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.choco_music.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import com.example.choco_music.R;
+import com.example.choco_music.adapters.ViewPagerAdpater;
+import com.google.android.material.tabs.TabLayout;
+
+import static com.example.choco_music.R.id.viewpager;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager= findViewById(viewpager);
+        ViewPagerAdpater viewPagerAdpater= new ViewPagerAdpater(getSupportFragmentManager());
+
+        viewPager.setAdapter(viewPagerAdpater);
+
+        TabLayout tabLayout=findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("HOME");
+        tabLayout.getTabAt(1).setText("CHART");
+        tabLayout.getTabAt(2).setText("SEARCH");
+        tabLayout.getTabAt(3).setText("PLAYLIST");
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/choco_music/adapters/ViewPagerAdpater.java
@@ -0,0 +1,46 @@
+package com.example.choco_music.adapters;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.choco_music.fragments.Chart_Fragment;
+import com.example.choco_music.fragments.Home_Fragment;
+import com.example.choco_music.fragments.Playlist_Fragment;
+import com.example.choco_music.fragments.Search_Fragment;
+
+public class ViewPagerAdpater extends FragmentPagerAdapter {
+
+    Home_Fragment f1;
+    Chart_Fragment f2;
+    Search_Fragment f3;
+    Playlist_Fragment f4;
+
+    public ViewPagerAdpater(FragmentManager fm) {
+        super(fm);
+
+        f1= new Home_Fragment();
+        f2= new Chart_Fragment();
+        f3= new Search_Fragment();
+        f4= new Playlist_Fragment();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position==0)
+            return f1;
+        else if(position==1)
+            return f2;
+        else if(position==2)
+            return f3;
+        else if(position==3)
+            return f4;
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
